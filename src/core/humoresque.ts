71a8758ae4ffa82cd28ca3fc60618e81545d0156
drevnii <ssamozhen13@gmail.com>
2021-08-25 19:18:26 +0400

    Update humoresque.ts

--- src/core/humoresque.ts
+++ src/core/humoresque.ts
@@ -1,26 +1,7 @@
-// function getRandomHumoresqueFromArray(arr: string[], length: number): string {
-//   const result = arr[getRandomNumber(0, arr.length - 1)];
-//   if (result.split(" ").length > length) {
-//     return getRandomHumoresqueFromArray(arr, length);
-//   } else {
-//     return result;
-//   }
-// }
-
-import { getHumoresqueKeyboard } from "../bot/keyboard";
 import { sendHumoresqueMessage } from "../bot/send";
 import { getPairRandomNumberFromRange } from "../misc/utils";
 import { IHumoresquesResult, ITask } from "../worker/models";
 import { addTaskToQueue } from "../worker/workerInit";
-
-// export async function getCustomHumoresque(args: number[]) {
-//   const first = (await getHumoresque()).split(" ");
-//   const second = (await getHumoresque()).split(" ");
-//   return first
-//     .slice(0, Math.floor((first.length / 100) * args[0]))
-//     .concat(second.slice(Math.floor((second.length / 100) * args[1])))
-//     .join(" ");
-// }
 
 export async function getCustomHumoresque(args: number[], chatId: number) {
   addTaskToQueue({ type: "humoresque", chatId, args });
