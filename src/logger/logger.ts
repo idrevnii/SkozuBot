f79894c980387018a2e571e8c71abba6269b024e
drevnii <ssamozhen13@gmail.com>
2021-12-30 02:04:32 +0400

    Fixed logger

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -4,7 +4,7 @@ export const logger = winston.createLogger({
   level: "http",
   defaultMeta: { service: "SkozuBot" },
   transports: [
-    new winston.transports.File({ filename: "error.log", level: "error" }),
-    new winston.transports.File({ filename: "combined.log" }),
+    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
+    new winston.transports.File({ filename: "logs/combined.log" }),
   ],
 });
