e39ce15c8c5c04cce69ffcb79a478418a9125488
drevnii <ssamozhen13@gmail.com>
2022-01-02 17:19:33 +0400

    testci

--- src/worker/worker.ts
+++ src/worker/worker.ts
@@ -2,7 +2,7 @@ import Bull from "bull";
 import { parentPort } from "worker_threads";
 import { getHumoresquesFromRandomPage } from "../api/humoresqueScrapper";
 
-const humoresques = new Bull("humoresques", process.env.REDIS_URL || "");
+const humoresques = new Bull("humoresquess", process.env.REDIS_URL || "");
 
 humoresques.process(async (job, done) => {
   const result = await getHumoresquesFromRandomPage();
