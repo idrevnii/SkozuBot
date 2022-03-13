6a58506117c2facfcdb185a1e7c182ca08de7726
Timofey Arkusha <justicecurcian@gmail.com>
2022-03-14 02:12:25 +0300

    Мы, как нидерландская компания приостанавливаем R&D на территории России

--- src/bot/keyboard.ts
+++
@@ -1,7 +0,0 @@
-import { Extra, Markup } from "telegraf";
-
-export function getHumoresqueKeyboard(callbackData: string) {
-  return Extra.markup((m) =>
-    m.inlineKeyboard([m.callbackButton("Еще", callbackData)])
-  );
-}
--- src/core/sabbath.ts
+++
@@ -1,6 +0,0 @@
-import { DateTime } from "luxon";
-
-export function getRemainingDaysUntillSabbath() {
-  const today = DateTime.now().setZone("Europe/Moscow").weekday;
-  return today === 7 ? 6 : 6 - today;
-}
